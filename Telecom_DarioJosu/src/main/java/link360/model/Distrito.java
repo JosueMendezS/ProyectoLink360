8e9021e48a874e0623cc125b62328a123935a688
Darío Mora Piedra <morapiedradari32@gmail.com>
2026-06-28 12:33:27 -0600

    Añado las clases DAO de Cliente y distrido, además en modelo agregué las clases de Distrito y Promoción

---
+++ Telecom_DarioJosu/src/main/java/link360/model/Distrito.java
@@ -0,0 +1,17 @@
+package link360.model;
+
+public class Distrito {
+    private int codDistrito;
+    private String nombreDistrito;
+    private int codCanton;
+
+    public Distrito(int codDistrito, String nombreDistrito, int codCanton) {
+        this.codDistrito = codDistrito;
+        this.nombreDistrito = nombreDistrito;
+        this.codCanton = codCanton;
+    }
+    public int    getCodDistrito()    { return codDistrito; }
+    public String getNombreDistrito() { return nombreDistrito; }
+    public int    getCodCanton()      { return codCanton; }
+    @Override public String toString() { return codDistrito + " – " + nombreDistrito; }
+}
